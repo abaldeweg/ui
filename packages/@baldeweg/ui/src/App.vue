25f0a8b9e93f2e74264f00f01c2ededc83824b18
André Baldeweg <kontakt@andrebaldeweg.de>
2022-05-16 15:01:16 +0200

    refactor App

--- packages/@baldeweg/ui/src/App.vue
+++ packages/@baldeweg/ui/src/App.vue
@@ -1,64 +1,56 @@
 <script setup>
-import BApp from '@/components/BApp/BApp.vue'
-import BForm from '@/components/BForm/BForm.vue'
-import BFormSelect from '@/components/BForm/BFormSelect.vue'
-import { getTheme, setTheme } from './services/theme'
-import { reactive, watch } from 'vue'
+import { useTheme } from './composables/useTheme.js'
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 
-const state = reactive({
-  position: 'bottom',
-  themes: [
-    { key: 'light', value: 'Light' },
-    { key: 'dark', value: 'Dark' },
-  ],
-  theme: getTheme() || 'light',
-})
+const { theme } = useTheme()
+const position = ref('bottom')
 
 const togglePosition = () => {
-  state.position = state.position === 'top' ? 'bottom' : 'top'
+  position.value = position.value === 'top' ? 'bottom' : 'top'
 }
-
-watch(
-  () => state.theme,
-  (theme) => {
-    setTheme(theme)
-  }
-)
 </script>
 
 <template>
-  <b-app id="app">
+  <BApp id="app">
     <RouterView />
+
     <div
-      class="components"
+      class="admin"
       :class="{
-        isTop: state.position === 'top',
-        isBottom: state.position === 'bottom',
+        isTop: position === 'top',
+        isBottom: position === 'bottom',
       }"
     >
-      <p><router-link :to="{ name: 'index' }">Home</router-link></p>
-      <p><button @click="togglePosition">Top/Bottom</button></p>
-      <b-form @submit.prevent>
-        <div class="form_group">
-          <div class="form_item">
-            <label for="theme" class="form_label visuallyHidden">Theme</label>
-          </div>
-          <div class="form_item">
-            <b-form-select
+      <p>
+        <RouterLink :to="{ name: 'index' }">Home</RouterLink>
+      </p>
+      <p>
+        <BButton design="outline" @click="togglePosition">Top/Bottom</BButton>
+      </p>
+      <BForm @submit.prevent>
+        <BFormGroup>
+          <BFormItem>
+            <BFormLabel for="theme" hidden>Theme</BFormLabel>
+          </BFormItem>
+          <BFormItem>
+            <BFormSelect
               id="theme"
-              v-model="state.theme"
-              :items="state.themes"
+              v-model="theme"
+              :items="[
+                { key: 'light', value: 'Light' },
+                { key: 'dark', value: 'Dark' },
+              ]"
             />
-          </div>
-        </div>
-      </b-form>
+          </BFormItem>
+        </BFormGroup>
+      </BForm>
     </div>
-  </b-app>
+  </BApp>
 </template>
 
 <style scoped>
-.components {
+.admin {
   position: fixed;
   right: 0;
   width: 200px;
@@ -70,10 +62,10 @@ watch(
   box-sizing: border-box;
   z-index: 99999;
 }
-.components.isTop {
+.admin.isTop {
   top: 0;
 }
-.components.isBottom {
+.admin.isBottom {
   bottom: 0;
 }
 </style>
