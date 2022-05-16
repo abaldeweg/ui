59bb3a57340260a4e6865f60b40873909ded3dee
André Baldeweg <kontakt@andrebaldeweg.de>
2022-05-16 15:04:59 +0200

    improve

--- packages/@baldeweg/ui/.eslintrc.cjs
+++ packages/@baldeweg/ui/.eslintrc.cjs
@@ -1,35 +1,35 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
         semi: false,
-        trailingComma: "es5",
+        trailingComma: 'es5',
       },
     ],
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
   ],
   env: {
-    "vue/setup-compiler-macros": true,
+    'vue/setup-compiler-macros': true,
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.spec.{js,ts,jsx,tsx}",
-        "cypress/integration/**.spec.{js,ts,jsx,tsx}",
+        '**/__tests__/*.spec.{js,ts,jsx,tsx}',
+        'cypress/integration/**.spec.{js,ts,jsx,tsx}',
       ],
-      extends: ["plugin:cypress/recommended"],
+      extends: ['plugin:cypress/recommended'],
     },
   ],
-};
+}
