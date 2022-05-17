ea5bab484d8c144f9e107c3f256a6eee639fd6e5
André Baldeweg <kontakt@andrebaldeweg.de>
2022-05-17 11:17:55 +0200

    allow previous env prefix

--- packages/@baldeweg/ui/vite.config.js
+++ packages/@baldeweg/ui/vite.config.js
@@ -27,4 +27,5 @@ export default defineConfig({
       },
     },
   },
+  envPrefix: ['VITE_', 'VUE_APP_'],
 })
--- packages/@baldeweg/web-demo/vite.config.js
+++ packages/@baldeweg/web-demo/vite.config.js
@@ -11,5 +11,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
     dedupe: ['vue'],
+    envPrefix: ['VITE_', 'VUE_APP_'],
   },
 })
