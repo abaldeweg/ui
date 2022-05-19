613fe9f86d53ad428d10f73feb5d0cf34c696151
André Baldeweg <kontakt@andrebaldeweg.de>
2022-05-19 11:40:03 +0200

    add pwa

--- packages/@baldeweg/web-demo/vite.config.js
+++ packages/@baldeweg/web-demo/vite.config.js
@@ -1,11 +1,39 @@
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: '@baldeweg/web-demo',
+        short_name: 'web-demo',
+        description: 'This is a demo app.',
+        theme_color: '#d7621d',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
