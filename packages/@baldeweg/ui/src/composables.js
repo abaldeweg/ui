e763ada09e90414e01892e02481c1388cce750f4
André Baldeweg <a@baldeweg.org>
2022-05-21 22:26:36 +0200

    export composables

---
+++ packages/@baldeweg/ui/src/composables.js
@@ -0,0 +1,4 @@
+export * from './composables/useTheme.js'
+export * from './composables/useTitle.js'
+export * from './composables/useToast.js'
+export * from './composables/useLocale.js'
--- packages/@baldeweg/ui/src/ui.js
+++ packages/@baldeweg/ui/src/ui.js
@@ -1,8 +1,4 @@
 import { components } from './components/index.js'
-import { useTheme } from './composables/useTheme.js'
-import { useTitle } from './composables/useTitle.js'
-import { useToast } from './composables/useToast.js'
-import { useLocale } from './composables/useLocale.js'
 
 export const createUi = () => {
   const install = (app) => {
@@ -14,4 +10,4 @@ export const createUi = () => {
   return { install }
 }
 
-export { useTheme, useTitle, useToast, useLocale }
+export * from './composables.js'
