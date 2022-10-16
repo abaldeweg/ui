d71a13c9f863e5eeea8ddbc6d3b812172b7f2014
André Baldeweg <a@baldeweg.org>
2022-10-16 14:06:31 +0000

    Remove tests

--- packages/@baldeweg/ui/src/components/__tests__/BFlexCol.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-import BFlexCol from '../BFlex/BFlexCol.vue'
-
-describe('BFlexCol', () => {
-  it('shows BFlexCol', () => {
-    expect(BFlexCol).toBeTruthy()
-  })
-})
--- packages/@baldeweg/ui/src/components/__tests__/BFlexRow.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-import BFlexRow from '../BFlex/BFlexRow.vue'
-
-describe('BFlexRow', () => {
-  it('shows BFlexRow', () => {
-    expect(BFlexRow).toBeTruthy()
-  })
-})
