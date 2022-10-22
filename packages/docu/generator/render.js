0de47d79a57309b838e244153aa326ae44c45d52
André Baldeweg <a@baldeweg.org>
2022-10-22 17:06:18 +0000

    Add ts

--- packages/docu/generator/render.js
+++ packages/docu/generator/render.js
@@ -1,22 +1,19 @@
 #!/usr/bin/env node
-
-'use strict'
-
-const fs = require('fs')
-const ejs = require('ejs')
-const { components } = require('./../json/components.json')
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const ejs_1 = __importDefault(require("ejs"));
+const components_json_1 = require("../json/components.json");
 const renderFile = (template, filename) => {
-  ejs.renderFile(template, { components }, {}, (_err, str) => {
-    fs.writeFile(filename, str, (err) => {
-      if (err) console.error(err)
-    })
-  })
-}
-
-renderFile(
-  __dirname + '/templates/Component.ejs',
-  './components.md',
-)
-
-console.log('Docu was rendered')
+    ejs_1.default.renderFile(template, { components: components_json_1.components }, {}, (_err, str) => {
+        fs_1.default.writeFile(filename, str, (err) => {
+            if (err)
+                console.error(err);
+        });
+    });
+};
+renderFile(__dirname + '/templates/Component.ejs', './components.md');
+console.log('Docu was rendered');
---
+++ packages/docu/generator/render.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import fs from 'fs'
+import ejs from 'ejs'
+import { components } from '../json/components.json'
+
+const renderFile = (template: string, filename: string) => {
+  ejs.renderFile(template, { components }, {}, (_err, str) => {
+    fs.writeFile(filename, str, (err): void => {
+      if (err) console.error(err)
+    })
+  })
+}
+
+renderFile(
+  __dirname + '/templates/Component.ejs',
+  './components.md',
+)
+
+console.log('Docu was rendered')
