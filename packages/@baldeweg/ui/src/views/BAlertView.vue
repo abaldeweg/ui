fd3578cb647fbc269a7adb31d5f549b9579212c6
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-04-18 20:29:07 +0000

    div

--- packages/@baldeweg/ui/src/views/BAlertView.vue
+++ packages/@baldeweg/ui/src/views/BAlertView.vue
@@ -5,10 +5,10 @@ useTitle({ title: 'BAlert - @baldeweg/ui' })
 </script>
 
 <template>
-  <article :style="{ margin: '20px' }">
+  <div :style="{ margin: '20px' }">
     <BAlert>Alert</BAlert>
     <BAlert type="error" hidable>Alert</BAlert>
     <BAlert type="warning" hidable>Alert</BAlert>
     <BAlert type="success" hidable>Alert</BAlert>
-  </article>
+  </div>
 </template>
