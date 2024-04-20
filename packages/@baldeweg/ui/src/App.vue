7e0cc21762b7deb742697d9f73a3ce78fb90854b
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-04-20 17:48:10 +0000

    Admin Box

--- packages/@baldeweg/ui/src/App.vue
+++ packages/@baldeweg/ui/src/App.vue
@@ -7,9 +7,16 @@ import { useLocale } from './composables/useLocale.js'
 useLocale()
 
 const el = ref(null)
+const handle = ref(null)
+
+const persistPosition = (pos) =>{
+  localStorage.setItem('admin_position', JSON.stringify(pos))
+}
 
 const { style } = useDraggable(el, {
-  initialValue: { x: 20, y: 500 },
+  initialValue: JSON.parse(localStorage.getItem('admin_position')),
+  handle: handle,
+  onEnd: persistPosition
 })
 </script>
 
@@ -18,9 +25,12 @@ const { style } = useDraggable(el, {
     <RouterView />
 
     <div class="admin" ref="el" :style="style">
-      <p>
-        <RouterLink :to="{ name: 'index' }">Home</RouterLink>
-      </p>
+      <div class="admin_header" ref="handle">Admin</div>
+      <div class="admin_body">
+        <p>
+          <RouterLink :to="{ name: 'index' }">Home</RouterLink>
+        </p>
+      </div>
     </div>
   </BApp>
 </template>
@@ -32,9 +42,17 @@ const { style } = useDraggable(el, {
   border: 1px solid var(--color-neutral-04);
   border-radius: 10px;
   background: var(--color-neutral-00);
-  padding: 20px;
   margin: 20px;
   box-sizing: border-box;
   z-index: 99999;
+}
+.admin_header {
+  border-radius: 10px 10px 0 0;
+  background: var(--color-neutral-02);
+  padding: 5px 20px;
+  cursor: move;
+}
+.admin_body {
+  padding: 20px;
 }
 </style>
