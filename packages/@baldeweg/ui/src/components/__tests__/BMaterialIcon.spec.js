b847e9df455bcc411a6517676e9030d2ffff058b
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-10-15 13:50:50 +0000

    Add test for BMaterialIcon

---
+++ packages/@baldeweg/ui/src/components/__tests__/BMaterialIcon.spec.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+
+import BMaterialIcon from '../BMaterialIcon/BMaterialIcon.vue'
+
+describe('BMaterialIcon', () => {
+  it('shows BMaterialIcon', () => {
+    expect(BMaterialIcon).toBeTruthy()
+  })
+})
--- packages/@baldeweg/ui/src/components/__tests__/BScrollToBottom.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-import BScrollToBottom from '../BScrollToBottom/BScrollToBottom.vue'
-
-describe('BScrollToBottom', () => {
-  it('shows BScrollToBottom', () => {
-    expect(BScrollToBottom).toBeTruthy()
-  })
-})
