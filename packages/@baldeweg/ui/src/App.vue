5c9af47b8a3b8d8c80c1887eb68d968296edd238
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-10-30 18:31:10 +0000

    remove admin container

--- packages/@baldeweg/ui/src/App.vue
+++ packages/@baldeweg/ui/src/App.vue
@@ -1,67 +1,19 @@
 <script setup>
-import { ref } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import { useDraggable } from '@vueuse/core'
+import { RouterView } from 'vue-router'
 import { useHead } from '@unhead/vue'
 
 useHead({
   title: "Home",
-  titleTemplate: '%s %separator %siteName',
-  templateParams: { separator: '-', siteName: '@baldeweg/ui' },
+  titleTemplate: '%s%separator%siteName',
+  templateParams: { separator: ' - ', siteName: '@baldeweg/ui' },
   htmlAttrs: {
     lang: navigator.language,
   }
-})
-
-const el = ref(null)
-const handle = ref(null)
-
-const persistPosition = (pos) => {
-  localStorage.setItem('admin_position', JSON.stringify(pos))
-}
-
-const { style } = useDraggable(el, {
-  initialValue: JSON.parse(localStorage.getItem('admin_position')),
-  handle: handle,
-  onEnd: persistPosition
 })
 </script>
 
 <template>
   <BApp id="app">
     <RouterView />
-
-    <div class="admin" ref="el" :style="style">
-      <div class="admin_header" ref="handle">Admin</div>
-      <div class="admin_body">
-        <p>
-          <RouterLink :to="{ name: 'index' }">Home</RouterLink>
-        </p>
-      </div>
-    </div>
   </BApp>
 </template>
-
-<style scoped>
-.admin {
-  position: fixed;
-  width: 200px;
-  border: 1px solid var(--color-neutral-04);
-  border-radius: 10px;
-  background: var(--color-neutral-00);
-  margin: 20px;
-  box-sizing: border-box;
-  z-index: 99999;
-}
-
-.admin_header {
-  border-radius: 10px 10px 0 0;
-  border-bottom: 1px solid var(--color-neutral-02);
-  padding: 5px 20px;
-  cursor: move;
-}
-
-.admin_body {
-  padding: 20px;
-}
-</style>
