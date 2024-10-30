0aa746e984396c454ea700d6604e4456ca0d20ec
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-10-30 19:27:37 +0000

    random text generator

--- packages/@baldeweg/ui/src/components/BModal/ExampleBModal.vue
+++ packages/@baldeweg/ui/src/components/BModal/ExampleBModal.vue
@@ -1,110 +1,17 @@
+<script setup>
+import { useRandomText } from '@/composables/useRandomText'
+
+const { generateParagraphs } = useRandomText()
+
+const text = generateParagraphs(5)
+</script>
+
 <template>
   <AppComponent component="BModal">
     <template #title>
       <BMaterialIcon :size="26">add</BMaterialIcon> Modal
     </template>
     <template #footer>Footer</template>
-    <BContainer>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-      <p>
-        Text Text Text Text Text Text Text Text Text Text Text Text Text Text
-        Text Text Text Text Text Text
-      </p>
-    </BContainer>
+    <BContainer v-html="text" />
   </AppComponent>
 </template>
