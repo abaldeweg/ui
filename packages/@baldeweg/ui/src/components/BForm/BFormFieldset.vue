f52a74d1d89c945c7cfa9e39e43bf369e3c060db
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-04-18 20:41:59 +0000

    add BFormFieldset component with template and styles

---
+++ packages/@baldeweg/ui/src/components/BForm/BFormFieldset.vue
@@ -0,0 +1,20 @@
+<template>
+  <fieldset class="fieldset">
+    <slot />
+  </fieldset>
+</template>
+
+<style>
+.fieldset {
+  clear: both;
+  border: 0 hidden;
+  padding: 0;
+  margin: 0;
+  margin-bottom: 20px;
+  overflow: auto;
+}
+
+.fieldset:last-child {
+  margin-bottom: 0;
+}
+</style>
