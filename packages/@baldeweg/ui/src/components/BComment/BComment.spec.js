5d78f5906a9bba0775c0a731522ff00b4a550ea6
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-04-26 13:45:13 +0000

    refactor: remove BComment component and related files

--- packages/@baldeweg/ui/src/components/BComment/BComment.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-import BComment from '../BComment/BComment.vue'
-
-describe('BComment', () => {
-  it('shows BComment', () => {
-    expect(BComment).toBeTruthy()
-  })
-})
