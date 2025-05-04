a7dab52d8ea2adf86986df0cddde28d1516cba06
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-05-04 15:37:17 +0000

    add playground

---
+++ packages/playground/eslint.config.ts
@@ -0,0 +1,17 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,vue}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  skipFormatting,
+)
---
+++ packages/playground/src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <h1 class="green">{{ msg }}</h1>
+  <p>
+    You’ve successfully created a new project with
+    <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> and
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+  </p>
+</template>
