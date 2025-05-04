1788222f10ee51d070c69f7063f74640cf9b6448
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-05-04 12:12:22 +0000

    fix

--- packages/@baldeweg/ui/src/ui.js
+++ packages/@baldeweg/ui/src/ui.js
@@ -1,17 +1,13 @@
-import { defineComponent } from 'vue'
-import components from './components/components.json'
+import components from './components/components.json';
 
 export const createUi = () => {
-  const install = (app, options) => {
-    components.forEach(component => {
-      app.component(
-        "B" + component.name,
-        defineComponent(() => import(`./components/B${component.directory}/B${component.name}.vue`))
-      )
+  const install = (app) => {
+    components.forEach((component) => {
+      const componentName = "B" + component.name;
+      const componentPath = `./components/B${component.directory}/B${component.name}.vue`;
+      app.component(componentName, () => import(componentPath));
+    });
+  };
 
-      app.provide("ui", options)
-    })
-  }
-
-  return { install }
-}
+  return { install };
+};
