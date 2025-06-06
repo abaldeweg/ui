79a0ce95b3df9d1c89ac15ff302c564ebf92a1e0
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-06-06 12:05:51 +0000

    feat: add BForm component stories for Storybook

---
+++ packages/@baldeweg/ui/src/components/BForm/BForm.stories.js
@@ -0,0 +1,23 @@
+import BForm from './BForm.vue';
+
+export default {
+  title: 'Components/BForm',
+  component: BForm,
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { BForm },
+  props: Object.keys(argTypes),
+  template: `
+    <BForm>
+      <p>Form content goes here</p>
+      <template #buttons>
+        <button type="submit">Submit</button>
+        <button type="button">Cancel</button>
+      </template>
+    </BForm>
+  `,
+});
+
+export const Default = Template.bind({});
+Default.args = {};
