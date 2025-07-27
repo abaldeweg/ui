bdddacdd1db40c014a3fae2b0ef952b22f765e8a
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-07-27 12:07:11 +0000

    remove playground

--- packages/playground/src/types/baldeweg-ui.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@baldeweg/ui' {
-  import { Plugin } from 'vue'
-  export function createUi(): Plugin
-}
--- packages/playground/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Welcome from '@/components/Welcome.vue'
-</script>
-
-<template>
-  <BContainer size="m">
-    <Welcome msg="Welcome" />
-  </BContainer>
-</template>
